f48c0eee12ac1def66d32e14cd1c9f8bc998ffc1
User <User@DESKTOP-FUSRB4G>
2025-10-01 11:30:10 +0300

    Add Triangle

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,6 +15,11 @@ public class Main {
         System.out.println(rectangle1.toString());
         System.out.println();
 
+        Triangle triangle1 = new Triangle();
+        System.out.println("Triangle 1:");
+        System.out.println(triangle1.toString());
+        System.out.println();
+
         GeometricObject myShape;
 
         myShape = new Circle(5.0, "red", true);
@@ -22,6 +27,10 @@ public class Main {
         System.out.println();
 
         myShape = new Rectangle(2.0, 3.0, "yellow", true);
+        System.out.println(myShape.toString());
+        System.out.println();
+
+        myShape = new Triangle(3.0, 4.0, 5.0, "blue", true);
         System.out.println(myShape.toString());
     }
 }
